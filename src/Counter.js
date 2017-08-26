20ea39d1b14f38bf0a4bc044edd471191c902dd5
somkiat <somkiat.p@gmail.com>
2017-08-26 22:12:52 +0700

    Add classname of result

--- src/Counter.js
+++ src/Counter.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react';
 class Counter extends Component {
   constructor(props) {
     super(props)
-    this.state = {counter: 0}
+    this.state = { counter: 0 }
   }
   render() {
-    const {counter} = this.state
-    const add = () => this.setState({counter: counter+1})
-    const minus = () => this.setState({counter: counter-1})
+    const { counter } = this.state
+    const add = () => this.setState({counter: counter + 1})
+    const minus = () => this.setState({counter: counter - 1})
     return (
       <div>
         <button className='add' onClick={add}>+</button>
-        <h1>{counter}</h1>
+        <h1 className='result'>{counter}</h1>
         <button className='minus' onClick={minus}>-</button>
       </div>
     );
--- src/Counter.spec.js
+++ src/Counter.spec.js
@@ -14,33 +14,33 @@ describe('<Counter/>', () => {
 
   it('ค่าเริ่มต้นต้องเป็น 0', () => {
     const component = shallow(<Counter/>)
-    expect(component.find('h1')).to.have.text('0')
+    expect(component.find('.result')).to.have.text('0')
   })
 
   it('ทำการเพิ่มค่าขึ้น 1', () => {
     const component = shallow(<Counter/>)
     component.find('button.add').simulate('click')
-    expect(component.find('h1')).to.have.text('1')
+    expect(component.find('.result')).to.have.text('1')
   })
 
   it('ทำการเพิ่มค่าขึ้น 2', () => {
     const component = shallow(<Counter/>)
     component.find('button.add').simulate('click')
     component.find('button.add').simulate('click')
-    expect(component.find('h1')).to.have.text('2')
+    expect(component.find('.result')).to.have.text('2')
   })
 
   it('ทำการลบค่าลง 1', () => {
     const component = shallow(<Counter/>)
     component.find('button.minus').simulate('click')
-    expect(component.find('h1')).to.have.text('-1')
+    expect(component.find('.result')).to.have.text('-1')
   })
 
   it('ทำการลบค่าลง 2', () => {
     const component = shallow(<Counter/>)
     component.find('button.minus').simulate('click')
     component.find('button.minus').simulate('click')
-    expect(component.find('h1')).to.have.text('-2')
+    expect(component.find('.result')).to.have.text('-2')
   })
 
   it('ทำการเพิ่มค่า 5 ครั้งและลบค่าสองครั้ง ต้องได้ค่า 3', () => {
@@ -52,7 +52,7 @@ describe('<Counter/>', () => {
     component.find('button.add').simulate('click')
     component.find('button.minus').simulate('click')
     component.find('button.minus').simulate('click')
-    expect(component.find('h1')).to.have.text('3')
+    expect(component.find('.result')).to.have.text('3')
   })
 
 })
