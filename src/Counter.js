b1c687f6e8cf87f574c03b5ae21b146900b382c3
somkiat <somkiat.p@gmail.com>
2017-09-03 18:13:01 +0700

    Update counter component with state

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import * as State from './state'
+import { INCREMENT, DECREMENT } from './actions'
 
 class Counter extends Component {
   constructor(props) {
     super(props)
-    this.state = { counter: 0 }
+    this.state = State.init()
   }
   render() {
     const { counter } = this.state
-    const add = () => this.setState({counter: counter + 1})
-    const minus = () => this.setState({counter: counter - 1})
+    const add = () => this.setState(State.update(INCREMENT, this.state))
+    const minus = () => this.setState(State.update(DECREMENT, this.state))
     return (
       <div>
         <button className='add' onClick={add}>+</button>
